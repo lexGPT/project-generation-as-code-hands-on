5af5033e834d4a8a601ad4fcb072c401cea40a1d
Lex Felix <112618115+lexfelixpost@users.noreply.github.com>
2024-06-20 19:50:17 +0200

    Altered project configuration

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,28 @@
-import { awscdk } from 'projen';
+import { JsonFile, awscdk } from 'projen';
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'project-generation-as-code',
   projenrcTs: true,
 
+  prettier: true,
+  prettierOptions:{
+    settings:{
+      singleQuote:true,
+    }
+  }
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.buildWorkflow?.addPostBuildJobCommands('postBuild', ['echo "This is a post build job"']);
+
+new JsonFile(project, "hands-on.json", {
+  obj: {
+    'hello': 'world'
+  }
+});
+
 project.synth();
